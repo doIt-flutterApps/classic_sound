1c40825468f7f4f4a3e2a7e1c13eb36b8eeef7d3
doIt-flutterApps <doit.flutterapps@gmail.com>
2025-03-20 12:33:20 +0900

    06-2-1

---
+++ android/app/src/main/kotlin/com/example2/classic_sound/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example2.classic_sound
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:classic_sound/admin/upload_page.dart';
+import 'package:classic_sound/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: UploadPage(),
+    );
+  }
+}
