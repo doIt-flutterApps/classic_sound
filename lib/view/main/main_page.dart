921ea53c1e5733bc0c3af55d6f5707df5b5e866c
doIt-flutterApps <doit.flutterapps@gmail.com>
2025-03-20 14:08:10 +0900

    06-4-1

--- lib/view/main/main_page.dart
+++ lib/view/main/main_page.dart
@@ -5,9 +5,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:classic_sound/view/main/sound/download_listtile.dart';
 import 'package:sqflite/sqflite.dart';
+import 'drawer_widget.dart';
 
 class MainPage extends StatefulWidget {
   final Database database;
+
   const MainPage({super.key, required this.database});
 
   @override
@@ -38,11 +40,12 @@ class _MainPageState extends State<MainPage> {
         title: const Text(Constant.APP_NAME),
         actions: [IconButton(onPressed: () async {}, icon: Icon(Icons.search))],
       ),
+      drawer: Drawer(child: DrawerWidget(database: widget.database)),
       body: ListView.builder(
         itemBuilder: (context, value) {
           Music music = Music.fromStoreData(documentList[value]);
           musicList.add(music);
-          return DownloadListTile(music: music, database: widget.database,);
+          return DownloadListTile(music: music, database: widget.database);
         },
         itemCount: documentList.length,
       ),
